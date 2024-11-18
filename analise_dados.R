5dccbeb2fc7246b71875d0a4c6f7e9bd3337cc78
pedro.bonifacio <pedro.bonifacio@loopit-consultoria.com>
2024-11-18 17:25:45 -0300

    Finalização do projeto número 1, gráficos finais e função para verificação de óbitos com regressão linear (98% de precisão)

--- analise_dados.R
+++ analise_dados.R
@@ -93,3 +93,80 @@ lines(density(df_campinas$obitos_novos), col="red")
 # Outro tipo
 qqnorm(df_campinas$obitos_novos)
 qqline(df_campinas$obitos_novos)
+
+### Correlação e regressão linear
+# Lembrando, dados paramétricos: normais, normalidade
+# Coeficiente de Pearson -> homogeneos e paramétricos
+# Correlação de Spearman -> volume grande de dados não paramétricos
+# Correlação de Kendall -> Volume pequeno de dados não paramétricos
+plot(df_campinas$casos, df_campinas$obitos)
+
+# Verificando que nessa variável não há normalidade
+qqnorm(df_campinas$obitos)
+qqline(df_campinas$obitos)
+
+# Como não há normalidade, precisamos usar Spearman ou Kendall, como é grande vamos de spearman
+cor(df_campinas$casos, df_campinas$obitos, method="spearman")
+
+# Buscando a equação da reta de regressão
+# lm -> linear model
+# Formula -> y em x
+regressao <- lm(formula= obitos ~ casos, data=df_campinas)
+# [1] é o coeficiente linear e [2] é o angular
+regressao$coefficients
+# Chegaríamos a uma equação como: 0.03384923x+47.20146719
+# Montando uma função matemática para chegar ao resultado de óbitos com 3000 casos.
+regressao_funcao_matematica <- function(x) {
+  return (0.03384923 * x) + 47.20146719
+}
+# 1015, podendo verificar no plot a veracidade da informação
+regressao_funcao_matematica(30000) 
+
+# Para verificarmos a precisão dessa função, podemos rodar o summary da nossa regresão
+summary(regressao) # 0.9834 ou 98%
+
+### GRÁFICO DE LINHA COM AJUSTE DE RETA
+if(!require(ggplot2)) install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(data = df_campinas, mapping = aes(x = casos, y = obitos)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "red") +
+  theme_gray()
+  
+# Verificando a correlação da melhor forma
+if(!require(corrplot)) install.packages("corrplot")
+library(corrplot)
+
+matriz_cor <- cor(
+  x = df_campinas$casos, 
+  y = df_campinas[, c("obitos", "obitos_novos")], 
+  method = "spearman"
+)
+
+corrplot(matriz_cor, method="color")
+
+
+### Gráfico final de exploração com ggplot
+covid_cidades <- df %>% filter(municipio %in% c("Campinas", "Guarulhos", "Sorocaba"))
+
+ggplot(covid_cidades, aes(x = casos, y = obitos, color = municipio)) +
+  geom_line() +
+  labs(title="Evolução dos óbitos em função dos casos de COVID em 3 cidades",
+       x = "Casos",
+       y = "Óbitos", 
+       color = "Cidades") +
+  theme_classic()
+
+### Utilizando regressão para chegar ao numero de óbitos em 20000 casos nas 3 cidades
+funcao_regressao_obitos_cidade <- function(cidade, casos) {
+  df_regressao <- df %>% filter(municipio == cidade)
+  regressao <- lm(formula = obitos ~ casos, data = df_regressao)
+  coeficientes <- regressao$coefficients
+  
+  calculo <- coeficientes[2] * casos + coeficientes[1]
+  r_squared <- summary(regressao)$r.squared
+  
+  return(sprintf("%.2f óbitos com a precisão de %.4f", calculo, (r_squared * 100)))
+}
+funcao_regressao_obitos_cidade("Campinas", 60000)
