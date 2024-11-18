f1d5fa0364cb46f3eeea6dd564ad53dceafd1f7f
pedro.bonifacio <pedro.bonifacio@loopit-consultoria.com>
2024-11-17 22:08:38 -0300

    Testes de normalidade gráfica

--- analise_dados.R
+++ analise_dados.R
@@ -12,7 +12,6 @@ View(df)
 # Arrumando a coluna criada e a data
 glimpse(df)
 
-
 df["idoso%"] = df$idoso.
 df$`idoso%` = as.integer(gsub("%", "", df$`idoso%`))
 df$idoso. = NULL
@@ -27,11 +26,13 @@ cidade_df = function (nome_cidade) {
   return(df_cidade)
 }
 
+### FILTRAGEM
+
 # Fazendo filtro por cidade
 df_campinas = cidade_df("Campinas")
 df_garulhos = cidade_df("Guarulhos")
 
-# Análises estatísticas
+### ANÁLISES ESTATÍSTICAS
 summary(df_campinas$obitos)
 
 # Plot(eixo_x, eixo_y, ) -> De crescimento ou decrescimento, linha simples
@@ -40,3 +41,55 @@ plot(df_campinas$data, df_campinas$obitos, title("MÉDIA ÓBITOS"), col = "red",
 # Histograma -> frequências de óbitos
 df_campinas_outubro = df_campinas %>% filter(df_campinas$mes == 7)
 hist(df_campinas_outubro$obitos, col="purple")
+
+
+### MEDIDAS DE POSIÇÃO
+# Mínimo e máximo
+min(df_campinas$obitos_novos)
+min(df_campinas$casos_novos)
+max(df_campinas$obitos_novos)
+
+# Amplitude
+range(df_campinas$obitos_novos)
+max(df_campinas$obitos_novos) - min(df_campinas$obitos_novos)
+
+# Quartis
+quantile(df_campinas$obitos_novos)
+
+# Amplitude interquartil IQR (Q3-Q1)
+amp_iqr <- IQR(df_campinas$obitos_novos)
+
+# Limite superior e inferior
+# Outliners > limite superior; Outliners < limite inferior
+quartis <- quantile(df_campinas$obitos_novos)
+limite_superior <- quartis[3] + (1.5 * amp_iqr)
+limite_inferior <- quartis[1] - (1.5 * amp_iqr)
+# Chegando a -12 e 15, sabendo que números dentro desses são normais.
+
+# Verificando tudo com o plot do box plot para casos novos
+summary(df_campinas$obitos_novos)
+boxplot(df_campinas$obitos_novos)
+
+### Tratando outliners
+# Indentificando outliners
+if(!require(outliers)) install.packages("outliers") 
+library(outliers)
+
+# Removendo o maior outlier
+outlier_in_campinas <- outlier(df_campinas$obitos_novos)
+df_campinas <- df_campinas %>% filter(df_campinas$obitos_novos != outlier_in_campinas)
+
+# Removendo todos outliers
+df_campinas <- df_campinas %>% filter(df_campinas$obitos_novos >= limite_inferior &
+                                        df_campinas$obitos_novos <= limite_superior)
+boxplot(df_campinas$obitos_novos)
+
+### Testes de normalidade
+# Utilizamos para verificar se a varíavel é uma distribuição normal simétrica
+# Análise com gráficos
+hist(df_campinas$obitos_novos, probability = T, col="blue")
+lines(density(df_campinas$obitos_novos), col="red")
+
+# Outro tipo
+qqnorm(df_campinas$obitos_novos)
+qqline(df_campinas$obitos_novos)
